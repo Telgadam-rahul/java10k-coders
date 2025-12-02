a29712d6b8d068c61978d182f8bc043fbda2ab24
rahul <telgadamrahul@gmail.com>
2025-12-02 12:18:11 +0530

    updated

--- Sumofdigits.java
+++ Sumofdigits.java
@@ -7,14 +7,26 @@ while(num!=0){
     sum = rem+sum;
     num/=10;
 }
-    System.out.println(sum);
+    System.out.println("sum of digits is "+sum);
 }
+ public void  reverse(int num){
+    int rev = 0;
+    while(num!=0){
+        int rem = num%10;
+        rev = rev*10+rem;
+        num/=10;
+    }
+    System.out.println("the reverse number is "+rev);
+ }
 
 
 public static void main(String[] args) {
    Sumofdigits obj1 = new Sumofdigits();
    obj1.sumOfDigits(101);
    obj1.sumOfDigits(567);
+   obj1.reverse(721);
+   obj1.reverse(765);
+
   
 }
 }
